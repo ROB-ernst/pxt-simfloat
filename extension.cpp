33b028a311b12a42ea9b4be939176a26c8558fc7
Floris Ernst <ernst@rob.uni-luebeck.de>
2018-11-14 11:57:00 +0100

    Add functions

--- extension.cpp
+++ extension.cpp
@@ -6,10 +6,32 @@ namespace sfloat {
     /**
     * Perform soft-float conversion
     */
-    //% blockId=sfloat_IntToSFloat block="Convert integer %x to SoftFloat representation"
-    int IntToSFloat(int x) {
-        simulate_float res = int_to_sfloat(x);
-		return res;
+    //% blockId=sfloat_fromInt block="Convert to SoftFloat %x"
+    int fromInt(int x) {
+        return int_to_sfloat(x);
     }
-
+	
+	//% blockId=sfloat_add block="Add two SoftFloats %x %y"
+	int add(int x, int y)
+	{
+		return sfloat_add(x, y);
+	}
+	
+	//% blockId=sfloat_add block="Multiply two SoftFloats %x %y"
+	int multipy(int x, int y)
+	{
+		return sfloat_multiply(x, y);
+	}
+	
+	//% blockId=sfloat_add block="Subtract two SoftFloats %x %y"
+	int minus(int x, int y)
+	{
+		return sfloat_minus(x, y);
+	}
+	
+	//% blockId=sfloat_add block="Divide two SoftFloats %x %y"
+	int div(int x, int y)
+	{
+		return sfloat_divide(x, y);
+	}
 }
