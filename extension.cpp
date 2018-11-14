339c0d55c82cbaf80df43641418fa967448cde36
ROB-ernst <ernst@rob.uni-luebeck.de>
2018-11-14 12:00:56 +0100

    Updates.

--- extension.cpp
+++ extension.cpp
@@ -6,10 +6,9 @@ namespace sfloat {
     /**
     * Perform soft-float conversion
     */
-    //% blockId=sfloat_IntToSFloat block="Convert integer %x to SoftFloat representation"
-    int IntToSFloat(int x) {
+    //% blockId=sfloat_fromInt block="Convert integer %x to SoftFloat representation"
+    int fromInt(int x) {
         simulate_float res = int_to_sfloat(x);
 		return res;
     }
-
 }
