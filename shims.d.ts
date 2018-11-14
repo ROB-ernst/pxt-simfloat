03335ea8e14c809946626268268e6e5f22895889
Floris Ernst <ernst@rob.uni-luebeck.de>
2018-11-14 12:24:57 +0100

    Fixed shim

--- shims.d.ts
+++ shims.d.ts
@@ -10,25 +10,25 @@ declare namespace sfloat {
     /**
      * Add two soft floats
      */
-    //% blockId=sfloat_add block="Add two SoftFloats %x %y" shim=sfloat::add
+    //% blockId=sfloat_add block="Add two SoftFloats x %x|and y %y" shim=sfloat::add
     function add(x: number, y: number): number;
 
     /**
      * Multiply two soft floats
      */
-    //% blockId=sfloat_multiply block="Multiply %x and %y" shim=sfloat::multiply
+    //% blockId=sfloat_multiply block="Multiply x %x|and y %y" shim=sfloat::multiply
     function multiply(x: number, y: number): number;
 
     /**
      * Subtract two soft floats
      */
-    //% blockId=sfloat_minus block="subtract %x from %y" shim=sfloat::minus
+    //% blockId=sfloat_minus block="subtract x %x|from y %y" shim=sfloat::minus
     function minus(x: number, y: number): number;
 
     /**
      * Divide two soft floats
      */
-    //% blockId=sfloat_div block="divide %x by %y" shim=sfloat::div
+    //% blockId=sfloat_div block="divide x %x|by y %y" shim=sfloat::div
     function div(x: number, y: number): number;
 }
 
